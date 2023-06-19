6b913701c61bcfba0f78d54ceefde211ccc20eae
Ismaeljpg <124393963+Ismaeljpg@users.noreply.github.com>
2023-06-19 19:24:34 -0300

    Add files via upload

---
+++ Full market/stock/Bicicleta/script/main.js
@@ -0,0 +1,33 @@
+const cantidadInput = document.getElementsByClassName('cantidad')[0];
+const totalSpan = document.querySelector('.total');
+
+cantidadInput.addEventListener('input', () => {
+  const cantidad = cantidadInput.value;
+  const precioUnitario = 15000;
+  const total = cantidad * precioUnitario;
+
+  totalSpan.textContent = '$' + total;
+});
+
+
+const botonComprar = document.getElementById("botonComprar");
+const ventanaEmergente = document.getElementById("ventanaEmergente");
+const fondo = document.getElementById("fondo");
+const botonAceptar = document.getElementById("botonAceptar");
+const botonCancelar = document.getElementById("botonCancelar");
+
+botonComprar.addEventListener("click", function() {
+  ventanaEmergente.style.display = "block";
+  fondo.style.display = "block";
+});
+
+botonAceptar.addEventListener("click", function() {
+  // Código para realizar la compra
+  ventanaEmergente.style.display = "none";
+  fondo.style.display = "none";
+});
+
+botonCancelar.addEventListener("click", function() {
+  ventanaEmergente.style.display = "none";
+  fondo.style.display = "none";
+});
